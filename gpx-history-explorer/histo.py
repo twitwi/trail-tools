22ec0170cc170b342b6a79e0b86df5cc1373fc88
Rémi Emonet <remi.emonet@reverse--com.heeere>
2020-04-12 14:15:45 +0200

    Simple inefficient prototype

---
+++ gpx-history-explorer/histo.py
@@ -0,0 +1,117 @@
+
+import vuejspython
+import numpy as np
+import subprocess
+from pathlib import Path
+import random
+import asyncio
+
+import gpx_parser as parser
+from scipy.sparse import dok_matrix
+
+
+def pairs_to_dict(l):
+    return {k:v for (k,v) in l}
+
+def max0(a):
+    if len(a) == 0: return 0
+    else: return np.max(a)
+
+
+def add_points_from_all(agg, step, gpx_files):
+    for gpx in gpx_files:
+        add_points(agg, step, gpx)
+
+def add_points(agg, step, gpx_path):
+    with open(gpx_path, 'r') as gpx_file:
+        gpx = parser.parse(gpx_file)
+    for (point, t, s, i) in gpx.walk():
+        y = int(point.latitude / step)
+        x = int(point.longitude / step)
+        #print(point.latitude, point.longitude)
+        agg[y, x] += 1
+    #print("{} tracks loaded".format(len(gpx)))
+    #for track in gpx:
+    #    print('Track with {} segments and {} points'.
+    #        format(len(track), track.get_points_no()))
+    #    for segment in track:
+    #        print('Segment with %s points' % len(segment))
+    #        for point in segment:
+    #            print(point)
+
+gpx202004 = "2020-04-01_18-12-43.gpx 2020-04-02_17-06-33.gpx 2020-04-03_18-24-53.gpx 2020-04-04_17-54-45.gpx 2020-04-05_14-40-36.gpx 2020-04-06_16-47-31.gpx 2020-04-07_17-31-36.gpx 2020-04-08_18-13-00.gpx 2020-04-09_17-00-11.gpx 2020-04-11_08-17-22.gpx 2020-04-10_08-18-37.gpx".split()
+gpx202003corona = "2020-03-24_07-36-50.gpx 2020-03-25_17-04-03.gpx 2020-03-26_16-45-03.gpx 2020-03-27_16-59-29.gpx 2020-03-28_17-20-13.gpx 2020-03-29_15-03-27.gpx 2020-03-30_17-47-48.gpx 2020-03-31_18-32-40.gpx".split()
+
+
+@vuejspython.model
+class App:
+    i = 0
+
+    # TODO across time
+    unit_m = 1e-5
+    l0step = 10 * unit_m
+    l0H = int(180/l0step) +1
+    l0W = int(2*l0H) +1
+    info = []
+    all_gpx = gpx202004 + gpx202003corona
+
+    view_x = 0
+    view_y = 0
+    view_w = 300
+    view_h = 200
+
+    def index(self, pos):
+        return int(pos / self.l0step)
+
+    def __init__(self):
+        self.l0 = dok_matrix((self.l0H, self.l0W)) #, dtype=np.int8)
+        add_points_from_all(self.l0, self.l0step, self.all_gpx)
+        self.info.append(self.l0.sum())
+        self.info.append(len(self.l0.nonzero()[0]))
+        self.view_y = self.index(45.430859)
+        self.view_x = self.index(4.3859)
+
+        self.l1 = dok_matrix((self.l0H//2, self.l0W//2)) #, dtype=np.int8)
+        add_points_from_all(self.l1, self.l0step*2, self.all_gpx)
+
+        self.l2 = dok_matrix((self.l0H//4, self.l0W//4)) #, dtype=np.int8)
+        add_points_from_all(self.l1, self.l0step*4, self.all_gpx)
+
+    def computed_image0(self):
+        arr = self.l0[self.view_y:self.view_y+self.view_h, self.view_x:self.view_x+self.view_w]
+        arr = arr[::-1, :]
+        res = [(int(u), int(v), arr[u,v]) for (i,(u,v)) in enumerate(zip(*(arr.nonzero())))] # sparse: list of (i,j,v)
+        return res
+
+    def computed_image1(self):
+        x = self.view_x//2
+        y = self.view_y//2
+        w = self.view_w//2
+        h = self.view_h//2
+        arr = self.l1[y:y+h,x:x+w]
+        arr = arr[::-1, :]
+        arr /= arr.tocsr().max()
+        res = [(int(u), int(v), int(arr[u,v]*100)) for (i,(u,v)) in enumerate(zip(*(arr.nonzero())))] # sparse: list of (i,j,v)
+        return res
+
+    def computed_image2(self):
+        x = self.view_x//4
+        y = self.view_y//4
+        w = self.view_w//4
+        h = self.view_h//4
+        arr = self.l1[y:y+h,x:x+w]
+        arr = arr[::-1, :]
+        arr /= arr.tocsr().max()
+        res = [(int(u), int(v), int(arr[u,v]*100)) for (i,(u,v)) in enumerate(zip(*(arr.nonzero())))] # sparse: list of (i,j,v)
+        return res
+
+    #@vuejspython.atomic # BUG
+    def offset_view(self, dx, dy):
+        print("##########",dx,dy)
+        self.view_x += dx
+        self.view_y += dy
+        #return ""
+
+vuejspython.start(App(), py_port=42989)
+
+
