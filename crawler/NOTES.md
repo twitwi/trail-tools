14f827a1482073835738451d307831c3292c73e8
Rémi Emonet <remi.emonet@reverse--com.heeere>
2024-10-08 16:03:59 +0200

    Pending crawler

--- crawler/NOTES.md
+++ crawler/NOTES.md
@@ -1,6 +1,51 @@
 
 
-# Livetrail
+# Scripts
+
+,,* files can be ignored
+
+Trying to remember what each script does.
+The scripts actually use xpath when requesting XML data.
+NB: I call strain some combination of distance plus D+.
+
+### livetrail_timedist.py
+
+This one is interesting to actually use the data.
+
+- accepts any number of table file(s)
+- accepts also parameters like BOF=516 to override some global variables
+- it is an example that collect all segments for all people in the race
+  - then plots as a function of strain (dist + D+), the negative time (in -hours) (higher is faster)
+  - also does a scatter plot of time at R1 vs time at the end (colored by at which ravito they drop off)
+
+### livetrail_digest.py
+
+Mostly like livetrail_timedist
+- (written before so utility functions might be slightly less advanced)
+- plots the speed (strain/h), to observe the decrease in speed during the race
+- tries 3 different strain coefficients (100, 150 and 200mD+/km)
+  - I don't remember but the goal was to find which coefficient is a best predictor across runners, and I think I was supposing that a bad coefficient will increase the variance of the speed, so I plot this variance...
+  - huge peaks in variance usually mean there is a wrong distance/d+ in the route description (or that there is a notable aid station ?)
+  
+
+
+### livetrail_get.py ((crawler))
+
+- receives a course_url (either full http... or just an livetrail id like utmb)
+- it possibly receives a second parameter which is the course id, in case the url is not containing the id, or we want to force it (as it is used for saving files), e.g., it is used to get the latest race but save its year, e.g. use python livetrail_get.py utmb utmb2022
+- it then gets the info file for the event
+- and for each subcourse (actual race), it gets the passages.php and stats.php
+- it also prints the history of previous events (when available)
+
+### livetrail_get_runners_for.py ((crawler))
+
+- receives table files (and possible VAR=... override)
+- it will create one file per runner with its information, including the itra id (not sure all races provide it)
+- there is a comment about how we (at the time at least) could request itra to get some stats
+
+
+
+# Notes Livetrail
 
 home of a race is data + link to a php-xsl that formats it
 where the subrace id is $e/@id (e.g. utmj, linx)
--- crawler/livetrail_timedist.py
+++ crawler/livetrail_timedist.py
@@ -17,6 +17,7 @@ def dict_sorted(d):
 #RacePoint = namedtuple('RacePoint', 'id dist dpos hfirst hlast')
 class RacePoint(NamedTuple):
     id: int
+    name: str
     dist: float
     dpos: float
     hfirst: str
@@ -27,6 +28,7 @@ def RacePoint_from_Element(el):
     #return ANamedTuple(*values)
     return RacePoint(
         int(a('idpt')),
+        a('n'),
         float(a('km')),
         float(a('d')),
         a('hp'),
@@ -111,6 +113,10 @@ def load_xml(path):
 BOF = '516' # bibs of focus
 BOI = '261 463 294 254 75 66' # bibs of interest
 RACE_TIME = 1 # set to 0 for hour of passage instead
+RELATIVE = 0 # relative time instead of absolute one (everyone takes 100h)
+EFFORT = 0
+FIRST_CHECKPOINT = 1
+LAST_CHECKPOINT = -1
 
 def go():
 
@@ -145,29 +151,55 @@ def go():
         xml = load_xml(table_file)
         bibs = xml.xpath('//lignes/l')
         strain_curves = []
+        # for ranking
+        ibof = 0
+        xbof = []
+        alllines = []
+        
         for ibib,bib in enumerate(bibs):
             segs = compute_segments(subpoints, bib, RACE_TIME)
-            at = 0 # first ravito, not checking its id actually
+            at = FIRST_CHECKPOINT - 1 # first ravito, not checking its id actually
             if len(segs) > at:
                 times_at_first.append(segs[at].cumul_dur)
-                times_at_last.append(segs[-1].cumul_dur)
+                try:
+                    times_at_last.append(segs[LAST_CHECKPOINT].cumul_dur)
+                except:
+                    times_at_last.append(segs[-1].cumul_dur)
                 colors.append(segs[0].idfrom * 100 + segs[-1].idto)
                 dosss.append(bib.attrib['doss'])
             x1s = np.array([s.cumul_dist for s in segs])
             x2s = np.array([s.cumul_strain(150) for s in segs])
+            if EFFORT:
+                usexs = x2s # effort
+            else:
+                usexs = x1s # distance
             ys = np.array([s.cumul_dur for s in segs])
+            if RELATIVE and ys.size>0:
+                ys /= ys[-1] / 100
             more = dict(alpha = 0.03)
             try:
                 lstyles = ['solid', 'dashed']
                 bnum = bib.attrib['doss']
                 if bnum in BOF.split(' '):
                     more = dict(alpha = .9, label=f"/ {bnum}", color="k", ls="dotted")
+                    ibof = ibib
+                    xbof = usexs
                 elif bnum in BOI.split(' '):
                     more = dict(alpha = .9, label=bnum, ls=lstyles[int(bnum)%2])
             finally: pass
             #plt.plot(x1s, ys, **more)
-            plt.plot(x2s, -ys, **more)
+            plt.plot(usexs, -ys, **more)
+            alllines.append(ys)
 
+    alllines.append(alllines.pop(ibof))
+    lmax = np.max([len(l) for l in alllines])
+    finishinglines = [l for l in alllines if len(l) == lmax]
+    print(len(alllines), len(finishinglines))
+    ranks = np.argsort(np.argsort(finishinglines, axis=0), axis=0)
+    #ranks = ranks[ranks[:,-1]>230] 
+    ranks = ranks[-1:]
+    plt.plot(xbof, ranks.T, alpha=0.5)
+            
     plt.grid()
     plt.legend(loc=0)
     plt.show()
@@ -182,11 +214,11 @@ def go():
                 plt.plot([times_at_first[i]]*2, minmax(times_at_last), c="k", lw=.5)
         except:
             print(f'bib {bnum} not found')
-    plt.xlabel("T Ravito 1")
-    plt.ylabel("T Final")
+    plt.xlabel(f'T First, {list(subpoints.values())[FIRST_CHECKPOINT].name}')
+    plt.ylabel(f'T Final, {list(subpoints.values())[LAST_CHECKPOINT].name})')
     plt.legend()
     plt.show()
 
     
-
-go()
+if __name__ == '__main__':
+    go()
